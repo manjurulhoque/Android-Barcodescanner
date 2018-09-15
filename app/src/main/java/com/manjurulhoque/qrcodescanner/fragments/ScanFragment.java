bd0ada9c4c885dc339afabfb7c28d5b8d70cbd05
manjurulhoque <rumimanzurulhoque@gmail.com>
2018-09-16 01:39:41 +0600

    Show scanned result in dialog

--- app/src/main/java/com/manjurulhoque/qrcodescanner/fragments/ScanFragment.java
+++ app/src/main/java/com/manjurulhoque/qrcodescanner/fragments/ScanFragment.java
@@ -2,11 +2,15 @@ package com.manjurulhoque.qrcodescanner.fragments;
 
 import android.app.Activity;
 import android.content.Context;
-import android.hardware.Camera;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,17 +21,16 @@ import com.manjurulhoque.qrcodescanner.R;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.google.zxing.BarcodeFormat;
-import com.google.zxing.Result;
-
+import me.dm7.barcodescanner.zbar.BarcodeFormat;
+import me.dm7.barcodescanner.zbar.Result;
 import me.dm7.barcodescanner.zbar.ZBarScannerView;
 import me.dm7.barcodescanner.zxing.ZXingScannerView;
 
-public class ScanFragment extends Fragment implements ZXingScannerView.ResultHandler {
+public class ScanFragment extends Fragment implements ZBarScannerView.ResultHandler, MessageDialogFragment.MessageDialogListener {
 
     private Activity mActivity;
     private Context mContext;
-    private ZXingScannerView zXingScannerView;
+    private ZBarScannerView zBarScannerView;
     private boolean mFlash;
     private boolean mAutoFocus;
     private ArrayList<Integer> mSelectedIndices;
@@ -42,7 +45,7 @@ public class ScanFragment extends Fragment implements ZXingScannerView.ResultHan
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mActivity = getActivity();
-        zXingScannerView = new ZXingScannerView(mActivity);
+        zBarScannerView = new ZBarScannerView(mActivity);
         setupFormats();
     }
 
@@ -51,7 +54,7 @@ public class ScanFragment extends Fragment implements ZXingScannerView.ResultHan
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_scan, container, false);
         contentFrame = view.findViewById(R.id.content_frame);
-        zXingScannerView = new ZXingScannerView(mActivity);
+        zBarScannerView = new ZBarScannerView(mActivity);
         if (savedInstanceState != null) {
             mFlash = savedInstanceState.getBoolean(FLASH_STATE, false);
             mAutoFocus = savedInstanceState.getBoolean(AUTO_FOCUS_STATE, true);
@@ -64,17 +67,17 @@ public class ScanFragment extends Fragment implements ZXingScannerView.ResultHan
             mCameraId = -1;
         }
         setupFormats();
-        contentFrame.addView(zXingScannerView);
+        contentFrame.addView(zBarScannerView);
         return view;
     }
 
     @Override
     public void onResume() {
         super.onResume();
-        zXingScannerView.setResultHandler(this); // Register ourselves as a handler for scan results.
-        zXingScannerView.startCamera(mCameraId);          // Start camera on resume
-        zXingScannerView.setFlash(mFlash);
-        zXingScannerView.setAutoFocus(mAutoFocus);
+        zBarScannerView.setResultHandler(this); // Register ourselves as a handler for scan results.
+        zBarScannerView.startCamera(mCameraId);          // Start camera on resume
+        zBarScannerView.setFlash(mFlash);
+        zBarScannerView.setAutoFocus(mAutoFocus);
     }
 
     @Override
@@ -89,28 +92,58 @@ public class ScanFragment extends Fragment implements ZXingScannerView.ResultHan
     @Override
     public void onPause() {
         super.onPause();
-        zXingScannerView.stopCamera(); // Stop camera on stop
+        zBarScannerView.stopCamera(); // Stop camera on stop
+        closeMessageDialog();
     }
 
     public void setupFormats() {
-        List<BarcodeFormat> formats = new ArrayList<>();
+        List<BarcodeFormat> formats = new ArrayList<BarcodeFormat>();
         if (mSelectedIndices == null || mSelectedIndices.isEmpty()) {
-            mSelectedIndices = new ArrayList<>();
-            for (int i = 0; i < ZXingScannerView.ALL_FORMATS.size(); i++) {
+            mSelectedIndices = new ArrayList<Integer>();
+            for (int i = 0; i < BarcodeFormat.ALL_FORMATS.size(); i++) {
                 mSelectedIndices.add(i);
             }
         }
 
         for (int index : mSelectedIndices) {
-            formats.add(ZXingScannerView.ALL_FORMATS.get(index));
+            formats.add(BarcodeFormat.ALL_FORMATS.get(index));
         }
-        if (zXingScannerView != null) {
-            zXingScannerView.setFormats(formats);
+        if (zBarScannerView != null) {
+            zBarScannerView.setFormats(formats);
         }
     }
 
     @Override
-    public void handleResult(Result result) {
-        Toast.makeText(getContext(), result.getText(), Toast.LENGTH_SHORT).show();
+    public void handleResult(Result rawResult) {
+        try {
+            Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+            Ringtone r = RingtoneManager.getRingtone(getActivity().getApplicationContext(), notification);
+            r.play();
+        } catch (Exception e) {
+        }
+        showMessageDialog(rawResult.getContents());
+    }
+
+    public void showMessageDialog(String message) {
+        DialogFragment fragment = MessageDialogFragment.newInstance("Scan Results", message, this);
+        fragment.show(getActivity().getSupportFragmentManager(), "scan_results");
+    }
+
+    public void closeMessageDialog() {
+        closeDialog("scan_results");
+    }
+
+    public void closeDialog(String dialogName) {
+        FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+        DialogFragment fragment = (DialogFragment) fragmentManager.findFragmentByTag(dialogName);
+        if (fragment != null) {
+            fragment.dismiss();
+        }
+    }
+
+    @Override
+    public void onDialogPositiveClick(DialogFragment dialog) {
+        // Resume the camera
+        zBarScannerView.resumeCameraPreview(this);
     }
 }
