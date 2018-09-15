c3383254342caa6ea7bd5d99f1863f81d1ece171
manjurulhoque <rumimanzurulhoque@gmail.com>
2018-09-16 00:30:51 +0600

    Scanner added

--- app/src/main/java/com/manjurulhoque/qrcodescanner/fragments/ScanFragment.java
+++ app/src/main/java/com/manjurulhoque/qrcodescanner/fragments/ScanFragment.java
@@ -1,5 +1,8 @@
 package com.manjurulhoque.qrcodescanner.fragments;
 
+import android.app.Activity;
+import android.content.Context;
+import android.hardware.Camera;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,16 +10,107 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.manjurulhoque.qrcodescanner.R;
 
-public class ScanFragment extends Fragment {
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.Result;
+
+import me.dm7.barcodescanner.zbar.ZBarScannerView;
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+public class ScanFragment extends Fragment implements ZXingScannerView.ResultHandler {
+
+    private Activity mActivity;
+    private Context mContext;
+    private ZXingScannerView zXingScannerView;
+    private boolean mFlash;
+    private boolean mAutoFocus;
+    private ArrayList<Integer> mSelectedIndices;
+    private int mCameraId = -1;
+    private static final String FLASH_STATE = "FLASH_STATE";
+    private static final String AUTO_FOCUS_STATE = "AUTO_FOCUS_STATE";
+    private static final String SELECTED_FORMATS = "SELECTED_FORMATS";
+    private static final String CAMERA_ID = "CAMERA_ID";
+    private ViewGroup contentFrame;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mActivity = getActivity();
+        zXingScannerView = new ZXingScannerView(mActivity);
+        setupFormats();
+    }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_scan, container, false);
-
+        contentFrame = view.findViewById(R.id.content_frame);
+        zXingScannerView = new ZXingScannerView(mActivity);
+        if (savedInstanceState != null) {
+            mFlash = savedInstanceState.getBoolean(FLASH_STATE, false);
+            mAutoFocus = savedInstanceState.getBoolean(AUTO_FOCUS_STATE, true);
+            mSelectedIndices = savedInstanceState.getIntegerArrayList(SELECTED_FORMATS);
+            mCameraId = savedInstanceState.getInt(CAMERA_ID, -1);
+        } else {
+            mFlash = false;
+            mAutoFocus = true;
+            mSelectedIndices = null;
+            mCameraId = -1;
+        }
+        setupFormats();
+        contentFrame.addView(zXingScannerView);
         return view;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        zXingScannerView.setResultHandler(this); // Register ourselves as a handler for scan results.
+        zXingScannerView.startCamera(mCameraId);          // Start camera on resume
+        zXingScannerView.setFlash(mFlash);
+        zXingScannerView.setAutoFocus(mAutoFocus);
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putBoolean(FLASH_STATE, mFlash);
+        outState.putBoolean(AUTO_FOCUS_STATE, mAutoFocus);
+        outState.putIntegerArrayList(SELECTED_FORMATS, mSelectedIndices);
+        outState.putInt(CAMERA_ID, mCameraId);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        zXingScannerView.stopCamera(); // Stop camera on stop
+    }
+
+    public void setupFormats() {
+        List<BarcodeFormat> formats = new ArrayList<>();
+        if (mSelectedIndices == null || mSelectedIndices.isEmpty()) {
+            mSelectedIndices = new ArrayList<>();
+            for (int i = 0; i < ZXingScannerView.ALL_FORMATS.size(); i++) {
+                mSelectedIndices.add(i);
+            }
+        }
+
+        for (int index : mSelectedIndices) {
+            formats.add(ZXingScannerView.ALL_FORMATS.get(index));
+        }
+        if (zXingScannerView != null) {
+            zXingScannerView.setFormats(formats);
+        }
+    }
+
+    @Override
+    public void handleResult(Result result) {
+        Toast.makeText(getContext(), result.getText(), Toast.LENGTH_SHORT).show();
     }
 }
