75ba4b056545b6585a0c189cfabea52055a7510f
manjurulhoque <rumimanzurulhoque@gmail.com>
2018-09-18 11:45:29 +0600

    Generate barcode

--- app/src/main/java/com/manjurulhoque/qrcodescanner/fragments/GenerateBarcodeFragment.java
+++ app/src/main/java/com/manjurulhoque/qrcodescanner/fragments/GenerateBarcodeFragment.java
@@ -1,22 +1,94 @@
 package com.manjurulhoque.qrcodescanner.fragments;
 
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
 
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
 import com.manjurulhoque.qrcodescanner.R;
 
+import java.util.Arrays;
+
 public class GenerateBarcodeFragment extends Fragment {
+
+    private EditText mInputText;
+    private ImageView mImageView;
+    private FloatingActionButton mSave;
+    private Activity mActivity;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_generate_barcode, container, false);
+        mActivity = getActivity();
+
+        mInputText = view.findViewById(R.id.inputText);
+        mImageView = view.findViewById(R.id.outputBitmap);
+        mSave = view.findViewById(R.id.save);
+
+
+        mInputText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if (charSequence.length() == 0) {
+                    mImageView.setImageResource(R.drawable.ic_placeholder);
+                } else {
+                    generateBarcode(charSequence.toString());
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
 
         return view;
+    }
+
+    private void generateBarcode(String s) {
+        MultiFormatWriter writer = new MultiFormatWriter();
+        String finalData = Uri.encode(s);
+
+        // Use 1 as the height of the matrix as this is a 1D Barcode.
+        BitMatrix bm = null;
+        try {
+            bm = writer.encode(finalData, BarcodeFormat.CODE_128, 1080, 1);
+        } catch (WriterException e) {
+            e.printStackTrace();
+        }
+        int bmWidth = bm.getWidth();
+
+        Bitmap imageBitmap = Bitmap.createBitmap(bmWidth, 640, Bitmap.Config.ARGB_8888);
+
+        for (int i = 0; i < bmWidth; i++) {
+            // Paint columns of width 1
+            int[] column = new int[640];
+            Arrays.fill(column, bm.get(i, 0) ? Color.BLACK : Color.WHITE);
+            imageBitmap.setPixels(column, 0, 1, i, 0, 1, 640);
+        }
+
+        mImageView.setImageBitmap(imageBitmap);
     }
 }
